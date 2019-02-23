fb0b11a82e8f46390017ab38f147f3c9e4d980da
Consolio <andrewbrycedavis@gmail.com>
2019-02-22 20:00:00 -0800

    First

---
+++ base/discode-client.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const Client = Discord.Client;
+const keyv = require('keyv');
+class DiscodeClient extends Client {
+    /**
+     * Create a new Discode client
+     * @constructor
+     * @param {string} token - The token to login with, can be obtained from https://discordapp.com/developers/applications/[botid]/bots and click 'Copy' under token
+     * @param {object} options - The options to initiate the client with, refer to https://discord.js.org/
+     */
+    constructor(token,options={}) {
+        super(options)
+        this.storage={}
+        this.storage.startOptions=options
+        this.auth.token=token
+        this.storage.persist
+    }
+
+    /**
+     * Run when ready
+     * @param {function} callback - Function to run once ready
+     */
+    ready(callback) {
+        this.on('ready',()=>{callback()})
+    }
+
+    /**
+     * Login to the Discode client
+     */
+    run() {
+        super.login(this.auth.token)
+    }
+}
+module.exports=DiscodeClient
